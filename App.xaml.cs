eb8647fda368ec4161070d88c7b174037a91a2f8
jonathanMora2005 <161750433+jonathanMora2005@users.noreply.github.com>
2024-09-18 15:42:24 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ex1Guiat
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
